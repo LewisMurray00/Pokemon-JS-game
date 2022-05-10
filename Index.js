11212177455f9e1c2576ffc4013925f06856de83
Lewis Murray <lewisdmurray00@gmail.com>
2022-05-10 21:41:13 +0100

    Played around with HTML Canvas, made a rectangle

--- Index.js
+++ Index.js
@@ -1,1 +1,11 @@
 const canvas = document.querySelector('canvas');
+
+/* Referencing canvas context */
+const context = canvas.getContext('2d')
+
+/* Setting the height and width of the canvas */
+canvas.height = 576;
+canvas.width = 1024;
+
+context.fillStyle = 'white'
+context.fillRect(0,0, canvas.width, canvas.height)
